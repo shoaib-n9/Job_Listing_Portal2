960dc65f3d63a5c777fdc57ab99bddba7389737f
shoaib-n9 <92673235+shoaib-n9@users.noreply.github.com>
2024-08-01 02:05:11 +0530

    added middlewares,routes(user.route.js) and route(API) in script.js

--- backend/script.js
+++ backend/script.js
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utilities/database.js';
+import userRoute from './routes/user.route.js';
+
 dotenv.config({}); //
 
 const app = express();
@@ -30,6 +32,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000; // IF EXIST PORT NO. WILL BE 8000 OTHERWISE 3000
+
+// API
+app.use('/api/v1/user', userRoute);
+
+/**
+"http://localhost:8000/api/v1/user/register"          
+"http://localhost:8000/api/v1/user/login"             THATS HOW API WILL WORK
+"http://localhost:8000/api/v1/user/profile/update"
+*/
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server running at PORT ${PORT}`);
