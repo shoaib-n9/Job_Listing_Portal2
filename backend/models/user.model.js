cb549d55797b97811bc1dd17b97bc2c8410a5d70
shoaib-n9 <92673235+shoaib-n9@users.noreply.github.com>
2024-08-03 00:36:40 +0530

    updated

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['student', 'Recruiter'],
+      enum: ['Student', 'Recruiter'],
       required: true,
     },
     profile: {
