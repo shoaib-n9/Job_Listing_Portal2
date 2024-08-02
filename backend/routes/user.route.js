d44828304b1d752b04436c9a1e6882576b5c51af
shoaib-n9 <92673235+shoaib-n9@users.noreply.github.com>
2024-08-03 01:05:22 +0530

    updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -10,11 +10,9 @@ import isAuthenticated from '../middlewares/isAuthenticated.js';
 
 const router = express.Router();
 
-router.route('/register').post(singleUpload, register);
+router.route('/register').post(register);
 router.route('/login').post(login);
 //router.route("/logout").get(logout);
-router
-  .route('/profile/update')
-  .post(isAuthenticated, singleUpload, updateProfile);
+router.route('/profile/update').post(isAuthenticated, updateProfile);
 
 export default router;
