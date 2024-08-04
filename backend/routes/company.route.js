c3ab5976671176136d0224d572fc0921bddfb868
shoaib-n9 <92673235+shoaib-n9@users.noreply.github.com>
2024-08-05 01:44:03 +0530

    updated company.route.js

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 import {
-  getCompanyById,
   getCompany,
+  getCompanyById,
   registerCompany,
   updateCompany,
 } from '../controllers/company.controller.js';
-//import { singleUpload } from "../middlewares/mutler.js";
+//import { singleUpload } from '../middlewares/mutler.js';
 
 const router = express.Router();
 
