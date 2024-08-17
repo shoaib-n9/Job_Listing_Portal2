761a2dc5901e6c8ac9840a8d4d67e8b58b23d888
shoaib-n9 <92673235+shoaib-n9@users.noreply.github.com>
2024-08-18 00:42:33 +0530

    added jobRoute

--- backend/script.js
+++ backend/script.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import connectDB from './utilities/database.js';
 import userRoute from './routes/user.route.js';
 import companyRoute from './routes/company.route.js';
+import jobRoute from './routes/job.route.js';
 
 dotenv.config({}); //
 
@@ -37,6 +38,7 @@ const PORT = process.env.PORT || 3000; // IF EXIST PORT NO. WILL BE 8000 OTHERWI
 // API
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/company', companyRoute);
+app.use('/api/v1/job', jobRoute);
 
 /**
 "http://localhost:8000/api/v1/user/register"          
